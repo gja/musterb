79f4bc3694b5e3b546b41acce7b68404d10b7fa6
Tejas Dinkar <tejas@gja.in>
2012-09-16 14:08:38 +0530

    First public release

--- lib/musterb/template_handler.rb
+++ lib/musterb/template_handler.rb
@@ -17,12 +17,12 @@ class Musterb::TemplateHandler < Musterb::Musterbifier
   def self.compile_mustache(source, options = {})
     erb = Musterb.to_erb(source, options.merge(:musterbifier_klass => self))
     klass = ActionView::Template::Handlers::ERB
-    klass.erb_implementation.new(erb, :trim => (klass.erb_trim_mode == "-")).src 
+    klass.erb_implementation.new(erb, :trim => (klass.erb_trim_mode == "-")).src
   end
 
   def self.call(template)
    options = {}
    options[:initial_context] = "initial_context" if template.locals.include? "initial_context"
-   compile_mustache(template.source, options)   
+   compile_mustache(template.source, options)
   end
 end
